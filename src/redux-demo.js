411c5d0a475aff466a8846bb7d8ba774032d1639
Brynner Ventura <bventura03@hotmail.com>
2022-06-16 00:13:36 -0400

    Added Decrement

--- src/redux-demo.js
+++ src/redux-demo.js
@@ -8,8 +8,14 @@ const counterReducer = (state = { counter: 0 }, action) => {
             }
     }
 
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
+}
+
     return state
-    
+
 }
 
 const store = redux.createStore(counterReducer)
@@ -24,3 +30,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber)
 
 store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
