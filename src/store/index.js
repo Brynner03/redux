0b8b9e47aeafc5e03d6670d0c5515ae1945b92d2
Brynner Ventura <bventura03@hotmail.com>
2022-06-16 01:03:43 -0400

    Never override current state, alway return a new object with new state

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,9 @@ const initialState = { counter: 0, showCounter: true}
 const counterReducer = (state = initialState , action) => {
     if (action.type === 'increment') {
         return {
+            // NEVER MUTATE EXISTING STATE, ALWAYS OVERRIDE IT && RETURN NEW STATE
             counter: state.counter + 1,
+            //  Always update every state when updating one state!
             showCounter: state.showCounter
         }
     }
